ba1a9583d486accdcae1f86984cc8b4978bfcc6a
amenibensaada <amenisaada19@gmail.com>
2021-01-26 20:35:14 +0100

    making card and receiving data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
 import "./index.css";
-
+import axios from "axios";
+import ShowCard from "./components/Card";
 
 function App() {
+  const [shows, setShows] = React.useState([]);
+  function getShows() {
+    axios
+      .get("https://api.tvmaze.com/shows")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        setShows(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+  useEffect(getShows, []);
+  console.log("shows",shows)
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-    
+    <div className="root">
+    {shows.map((show)=><ShowCard name={show.name} date={show.premiered} description={show.summary} image={show.image.original}/> )}
+    </div>
   );
 }
-
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+export default function ShowCard({name , date , image , description}) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  }; 
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={name} 
+        subheader={date}
+      />
+      <CardMedia
+        className={classes.media}
+        image={image}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {description}
+        </Typography>
+      </CardContent>
+  
+    </Card>
+  );
+}
