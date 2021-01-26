e53f3a093783794393096d4b48e1e7a3ca8b297b
amenibensaada <amenisaada19@gmail.com>
2021-01-26 18:47:01 +0100

    started implimanting the structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import "./index.css";
+
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    
+  );
+}
+
+
+export default App;
